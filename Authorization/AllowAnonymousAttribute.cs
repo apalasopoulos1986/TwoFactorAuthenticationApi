844b3560198c637fc1d18178c10b7a9357bb290a
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2022-04-28 14:57:58 +0300

    Add project files.

---
+++ Authorization/AllowAnonymousAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoFactorAuthenticationApi.Authorization
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AllowAnonymousAttribute : Attribute
+    {
+
+    }
+}
