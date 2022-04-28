f03ee7deed9e558db4b09663a36dfb900e727cce
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2022-04-28 15:39:53 +0300

    WIP-Adding data context etc

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TwoFactorAuthenticationApi.Entities;
+
+namespace TwoFactorAuthenticationApi.Helpers
+{
+    public class DataContext:DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("TwoFactorAuthenticationApiDatabase"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoFactorAuthenticationApi.Services
+{
+    public class UserService
+    {
+    }
+}
