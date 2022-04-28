2c9622dbc72417ff2cda7b40a8fd9eb97348b8ac
Anastasios Palasopoulos <57755419+apalasopoulos1986@users.noreply.github.com>
2022-04-28 16:46:12 +0300

    WIP-Configuring Services

--- Startup.cs
+++ Startup.cs
@@ -10,27 +10,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TwoFactorAuthenticationApi.Authorization;
+using TwoFactorAuthenticationApi.Helpers;
+using TwoFactorAuthenticationApi.Services;
 
 namespace TwoFactorAuthenticationApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _configuration;
+
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
         {
-            Configuration = configuration;
+            _env = env;
+            _configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
+        {   // use sql server db 
+            services.AddDbContext<DataContext>();
+            // use sql server db 
+            services.AddCors();
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TwoFactorAuthenticationApi", Version = "v1" });
             });
+            // configure strongly typed settings objects
+            services.Configure<AppSettings>(_configuration.GetSection("AppSettings"));
+
+            // configure DI for application services
+            services.AddScoped<IJwtUtils, JwtUtils>();
+            services.AddScoped<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
